a91fe9caadcad7e3e25b0bd59be6cd237330ad3f
Sebastian Malek <malek@malek.li>
2022-01-24 00:47:20 +0100

    add index template

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::response::content;
+
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct HomepageTemplate {
+
+}
+
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn root() -> content::Html<String> {
+    let template = HomepageTemplate { };
+
+    let response = content::Html(template.to_string());
+    response
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![hello])
+        .mount("/", routes![root])
 }

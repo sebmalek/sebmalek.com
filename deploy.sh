fc6c40f541cd9111e71cff632ace38ce33ce318a
Sebastian Malek <malek@malek.li>
2022-02-03 00:43:32 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# fail on error
+set -e
+
+if [[ $# -eq 0 ]] ; then
+    echo "Server missing"
+    exit 1
+fi
+
+# update deps
+cargo update
+
+cargo build --release -j6
+
+rm -rf /tmp/deploy
+mkdir /tmp/deploy
+
+cp Rocket.toml /tmp/deploy
+cp target/release/sebmalek-com /tmp/deploy
+cp -R static/ /tmp/deploy
+cp -R files/ /tmp/deploy
+cp -R templates/ /tmp/deploy
+
+rsync --progress -rvzh --exclude={'.gitkeep'} -e 'ssh -o StrictHostKeyChecking=no' /tmp/deploy/* $1:/opt/sebmalek-com
+
+ssh -t $1 "sudo systemctl restart sebmalek-com.service"
